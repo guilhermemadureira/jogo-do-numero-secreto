559dc70aa19b630e55d4423fd5b7a278c2afdfcd
Guilherme Madureira <guilhermemadureira789@gmail.com>
2025-05-18 09:50:50 -0300

    Alterado o numero limite para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaNumeroSorteados =[];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 /* let titulo = document.querySelector('h1');
@@ -17,7 +17,7 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1' , 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10');
+    exibirTextoNaTela('p', `Escolha um numero entre 1 e ${numeroLimite}`);
 }
 
 exibirMensagemInicial();
